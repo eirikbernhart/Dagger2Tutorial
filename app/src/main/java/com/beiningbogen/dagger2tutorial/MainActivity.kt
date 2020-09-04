78da988d5984515c512d0d07894cc762c1b8e2ee
Eirik Bernhart <eirik@beiningbogen.no>
2020-09-04 09:47:48 +0200

    Using "Field Injection"

--- app/src/main/java/com/beiningbogen/dagger2tutorial/MainActivity.kt
+++ app/src/main/java/com/beiningbogen/dagger2tutorial/MainActivity.kt
@@ -4,18 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.internal.DaggerCollections
 import timber.log.Timber
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var car: Car
+   @Inject
+   lateinit var car: Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val carComponent = DaggerCarComponent.create()
+        carComponent.inject(this)
 
-        car = carComponent.getCar()
         car.drive()
     }
 }
